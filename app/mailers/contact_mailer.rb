299a8ccfa1b675a3d5f7e47d85b4357d2b5846b9
Sowmya Sekar <sowmyas.57@gmail.com>
2017-06-13 22:24:48 +0000

    Made corrections on contact_mailer.rb

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,12 +1,11 @@
 class ContactMailer < ActionMailer::Base
     default to: 'sowmyas.57@gmail.com'
     
-    def contact_email (name, email, body)
+    def contact_email(name, email, body)
         @name = name
         @email = email
         @body = body
         
-        mail (from: email, subject: 'Contact Form Message')
-        
+        mail(from: email, subject: 'Contact Form Message')
     end
 end
